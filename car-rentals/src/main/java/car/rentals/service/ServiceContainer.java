95b0badf28f093189a5524010e2335b9e87fc228
Parkash Arjan <Arjan.Parkash.Git@gmail.com>
2020-09-23 22:59:29 -0400

    added

--- car-rentals/src/main/java/car/rentals/service/ServiceContainer.java
+++ car-rentals/src/main/java/car/rentals/service/ServiceContainer.java
@@ -51,7 +51,7 @@ public final class ServiceContainer {
 		if (carInventoryService == null) {
 			carInventoryService = new CarInventoryService();
 		}
-		return carInventoryService;
+		return carInventoryService; 
 
 	}
 
